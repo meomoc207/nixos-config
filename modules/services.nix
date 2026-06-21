3fcdcf48137688e1d553ce706e2b1b2701c9c84e
meomoc207 <vinnynghiem207@gmail.com>
2026-06-21 20:20:11 +1000

    made changes

--- modules/services.nix
+++ modules/services.nix
@@ -4,5 +4,16 @@
   services.udisks2.enable = true;
   services.openssh.enable = true;
   services.irqbalance.enable = true;
-  services.mpd-mpris.enable = true;
+  services.mpd = {
+  enable = true;
+  music_directory = "/home/lvsumi/Music";
+  settings = {
+    audio_output = [
+       {
+         type = "pipewire";
+         name = "PipeWire Output";
+       }
+     ];
+   };
+ };
 }
